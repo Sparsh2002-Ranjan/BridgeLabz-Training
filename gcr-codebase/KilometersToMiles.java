754574977dbcb3dd1d5383645c73ee5d0b25cc47
Sparsh Ranjan <ranjansparsh872@gmail.com>
2025-12-18 16:37:07 +0530

    Removed gcr-codebase folder

--- gcr-codebase/KilometersToMiles.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class KilometersToMiles {
-    public static void main(String[] args) {
-        double km=10.6;
-        System.out.println((km*1.6)+"miles");
-    }
-}
--- gcr-codebase/PerimeterOfSquare.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class PerimeterOfSquare {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int side=sc.nextInt();
-        int peri=4*side;
-        System.out.println("The length of the side is "+ side +" whose perimeter is "+peri);
-    }
-}
